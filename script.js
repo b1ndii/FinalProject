54042fab2dd82d8c2fcdeaac30d566a60e37dec7
b1ndii <bind.berisha1@gmail.com>
2025-05-24 12:34:09 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,74 @@
-var littleton = L.marker([42.65917322650542, 21.165693430509386]).bindPopup('Prishtina'),
-    denver    = L.marker([42.463389327478964, 21.469076579236056]).bindPopup('Gilan')
+var prishtina = L.marker([42.65917322650542, 21.165693430509386])
+  .bindPopup(`
+    <a style="text-decoration: none; color:black; display: block; text-align: center;" href="https://example.com/prishtina">
+      <b>
+        See the landmarks<br>
+        <span style="display: block;">of Prishtina</span>
+      </b>
+    </a>
+  `);
 
+    gilan = L.marker([42.463389327478964, 21.469076579236056])
+    .bindPopup(`
+    <a style="text-decoration: none; color:black; display: block; text-align: center;" href="https://example.com/prishtina">
+      <b>
+        See the landmarks<br>
+        <span style="display: block;">of Gilan</span>
+      </b>
+    </a>
+  `);
 
-var cities = L.layerGroup([littleton, denver]);
+  ferizaj = L.marker([42.37358381814714, 21.14639044611495])
+    .bindPopup(`
+    <a style="text-decoration: none; color:black; display: block; text-align: center;" href="https://example.com/prishtina">
+      <b>
+        See the landmarks<br>
+        <span style="display: block;">of Ferizaj</span>
+      </b>
+    </a>
+  `);
+
+  prizren = L.marker([42.22105646478453, 20.72822919601476])
+    .bindPopup(`
+    <a style="text-decoration: none; color:black; display: block; text-align: center;" href="https://example.com/prishtina">
+      <b>
+        See the landmarks<br>
+        <span style="display: block;">of Prizren</span>
+      </b>
+    </a>
+  `);
+
+  peja = L.marker([42.66260107998889, 20.286585844563216])
+    .bindPopup(`
+    <a style="text-decoration: none; color:black; display: block; text-align: center;" href="https://example.com/prishtina">
+      <b>
+        See the landmarks<br>
+        <span style="display: block;">of Peja</span>
+      </b>
+    </a>
+  `);
+
+  gjakova = L.marker([42.38436370552528, 20.427229896992653])
+    .bindPopup(`
+    <a style="text-decoration: none; color:black; display: block; text-align: center;" href="https://example.com/prishtina">
+      <b>
+        See the landmarks<br>
+        <span style="display: block;">of Gjakova</span>
+      </b>
+    </a>
+  `);
+
+  mitrovica = L.marker([42.89409771632134, 20.865916258926536])
+    .bindPopup(`
+    <a style="text-decoration: none; color:black; display: block; text-align: center;" href="https://example.com/prishtina">
+      <b>
+        See the landmarks<br>
+        <span style="display: block;">of Mitrovica</span>
+      </b>
+    </a>
+  `);
+
+var cities = L.layerGroup([prishtina, gilan, ferizaj, prizren, peja, gjakova, mitrovica]);
 
 var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -14,8 +80,8 @@ var osmHOT = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
     attribution: '© OpenStreetMap contributors, Tiles style by Humanitarian OpenStreetMap Team hosted by OpenStreetMap France'});
 
 var map = L.map('map', {
-    center: [42.65917322650542, 21.165693430509386],
-    zoom: 10,
+    center: [42.6026, 20.9020],
+    zoom: 9,
     layers: [osm, cities]
 });
 
